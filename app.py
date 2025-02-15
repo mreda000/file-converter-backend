59234e13855cec97ea419fd9a8eeb459c14926cc
Moe Red <moha.h.reda@gmail.com>
2025-02-15 12:24:14 -0500

    Fixed Flask server to work with Render

--- app.py
+++ app.py
@@ -16,4 +16,5 @@ def upload_file():
     return send_file(file.filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=10000)
+
